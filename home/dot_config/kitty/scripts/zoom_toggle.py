61a57efd81cebfa464966ac1a1120cc5fdbe0f28
prettycation <a2095578831@outlook.com>
2026-03-15 23:44:38 +0800

    feat(kitty): add kitty config, scripts, and themes - Adds basic kitty config, fonts, themes, and navigation scripts. - Updates .chezmoiignore to ignore kitty config while eq Windows.

---
+++ home/dot_config/kitty/scripts/zoom_toggle.py
@@ -0,0 +1,16 @@
+from kittens.tui.handler import result_handler
+from kitty.boss import Boss
+
+
+def main(args: list[str]) -> str:
+    pass
+
+
+@result_handler(no_ui=True)
+def handle_result(args: list[str], answer: str, target_window_id: int, boss: Boss) -> None:
+    tab = boss.active_tab
+    if tab is not None:
+        if tab.current_layout.name == 'stack':
+            tab.last_used_layout()
+        else:
+            tab.goto_layout('stack')
