927412c8ad9b142bed0514f7b7e95fa0fb0d0b3d
prettycation <a2095578831@outlook.com>
2026-04-09 19:50:58 +0800

    feat: add type def from wezterm-types

--- home/dot_config/wezterm/events/gui-startup.lua
+++ home/dot_config/wezterm/events/gui-startup.lua
@@ -1,13 +1,14 @@
--- local wezterm = require('wezterm')
--- local mux = wezterm.mux
+---@type Wezterm
+local wezterm = require('wezterm')
+local mux = wezterm.mux
 
 local M = {}
 
 M.setup = function()
-   -- wezterm.on('gui-startup', function(cmd)
-   --    local _, _, window = mux.spawn_window(cmd or {})
-   --    window:gui_window():maximize()
-   -- end)
+   wezterm.on('gui-startup', function(cmd)
+      local _, _, window = mux.spawn_window(cmd or {})
+      window:gui_window():maximize()
+   end)
 end
 
 return M
