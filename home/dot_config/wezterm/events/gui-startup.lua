97e37b396e75c2a00e5c68fad9d778fcf30620b7
prettycation <a2095578831@outlook.com>
2026-04-11 19:45:37 +0800

    refactor(wezterm/gui-startup): disable auto-maximize on GUI startup - Stops windows from maximizing automatically on launch

--- home/dot_config/wezterm/events/gui-startup.lua
+++ home/dot_config/wezterm/events/gui-startup.lua
@@ -1,14 +1,14 @@
 ---@type Wezterm
-local wezterm = require('wezterm')
-local mux = wezterm.mux
+-- local wezterm = require('wezterm')
+-- local mux = wezterm.mux
 
 local M = {}
 
 M.setup = function()
-   wezterm.on('gui-startup', function(cmd)
-      local _, _, window = mux.spawn_window(cmd or {})
-      window:gui_window():maximize()
-   end)
+   -- wezterm.on('gui-startup', function(cmd)
+   -- local _, _, window = mux.spawn_window(cmd or {})
+   -- window:gui_window():maximize()
+   -- end)
 end
 
 return M
