f2ddf53bd11ccbcb2d23e0a549a0f972488f7ae8
prettycation <a2095578831@outlook.com>
2026-04-19 22:07:00 +0800

    refactor(nvim): reorganize plugin files into exact_* directories
    - comment out jk keymaps in keymaps.lua

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -2,9 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "jk", "<Esc>", { desc = "退出插入模式", silent = true })
+-- vim.keymap.set("i", "jk", "<Esc>", { desc = "退出插入模式", silent = true })
 
-vim.keymap.set("c", "jk", "<Esc>", { desc = "退出命令模式", silent = true })
+-- vim.keymap.set("c", "jk", "<Esc>", { desc = "退出命令模式", silent = true })
 
 -- 禁用原生 diffget (do)
 vim.keymap.set("n", "do", "<Nop>", { noremap = true, silent = true })
